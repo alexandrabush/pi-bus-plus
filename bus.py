d467a09e8f09f6fffcd813400ba8873738ae408b
Alexandra Bush <alexandrabush1@gmail.com>
2017-11-29 20:11:30 -0500

    modified lcd test 2.0

--- bus.py
+++ bus.py
@@ -16,7 +16,7 @@ lcd_columns = 16
 lcd_rows = 2
 
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
-lcd.enable_display(enable)
+lcd.enable_display(True)
 lcd.message('Hello\nworld!')
 time.sleep(5.0)
 
