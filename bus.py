b7841e3408dd4097b9c5f2f461bd02992eee46db
Alexandra Bush <alexandrabush1@gmail.com>
2017-11-29 20:04:17 -0500

    added basic lcd write test

--- bus.py
+++ bus.py
@@ -4,6 +4,20 @@ from datetime import datetime, date, time
 import math
 import Adafruit_CharLCD as LCD
 
+#lcd setup
+lcd_rs        = 25 
+lcd_en        = 24
+lcd_d4        = 23
+lcd_d5        = 17
+lcd_d6        = 18
+lcd_d7        = 22
+lcd_backlight = 4
+lcd_columns = 16
+lcd_rows = 2
+
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+lcd.message('Hello\nworld!')
+
 
 #setup
 mykey = "KK6JMVmf2H3wSjkHXZ877bXWm"
