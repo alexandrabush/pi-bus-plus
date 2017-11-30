5b23204a43268a1911cec8693f0aefe7a127a333
Alexandra Bush <alexandrabush1@gmail.com>
2017-11-29 22:43:15 -0500

    added polling logic

--- bus.py
+++ bus.py
@@ -9,8 +9,6 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
-buttonState = GPIO.input(18)
-
 #lcd setup
 lcd_rs = 25
 lcd_en = 24
@@ -32,62 +30,57 @@ api = BustimeAPI(mykey)
 #stops
 murrayID = 7096
 starbucksID = 7233
-
-#get murray stop info
-print "getting murray stop info..."
-mydict = api.predictions(murrayID, maxpredictions=1)
-
-#get the time and calc the time until it arrives
-info = mydict["prd"]
-
-#set up the Murray stop
 mArrival = "N\A"
 mBus = "N\A"
-
-#grab the info i want
-for k,v in info.items():
-	#prdtm is the predicted arrival time
-	if "prdtm" == k:
-		mArrival = v
-	#rt is the bus 
-	if "rt" == k:
-		mBus = v
-
-
-#get starbucks stop info
-print "getting braddock stop info..."
-mydict = api.predictions(starbucksID, maxpredictions=1)
-
-info = mydict["prd"]
-
 sArrival = "N\A"
 sBus = "N\A"
 
-for k,v in info.items():
-	if "prdtm" == k:
-		sArrival = v
-	if "rt" == k:
-		sBus = v
+while True:
+	buttonState = GPIO.input(18)
+	if buttonState == False:
+		
+		#get murray stop info
+		mydict = api.predictions(murrayID, maxpredictions=1)
+		info = mydict["prd"]
 
-# get current time 
-cTime = datetime.now().time()
+		for k,v in info.items():
+			#prdtm is the predicted arrival time
+			if "prdtm" == k:
+				mArrival = v
+			#rt is the bus 
+			if "rt" == k:
+				mBus = v
 
-# convert times and calc # of minutes until arrival
-mArrival = datetime.strptime(mArrival, "%Y%m%d %H:%M:%S")
-sArrival = datetime.strptime(sArrival, "%Y%m%d %H:%M:%S")
 
-sminutes = datetime.combine(date.today(), sArrival.time()) - datetime.combine(date.today(), cTime)
-sminutes = math.floor(sminutes.total_seconds()/60)
-sminutes = math.trunc(sminutes)
+		#get shady stop info
+		mydict = api.predictions(starbucksID, maxpredictions=1)
+		info = mydict["prd"]
 
-mminutes = datetime.combine(date.today(), mArrival.time()) - datetime.combine(date.today(), cTime)
-mminutes = math.floor(mminutes.total_seconds()/60)
-mminutes = math.trunc(mminutes)
+		for k,v in info.items():
+			if "prdtm" == k:
+				sArrival = v
+			if "rt" == k:
+				sBus = v
 
-#print data
-lcd.set_cursor(0,0)
-lcd.message("Murray: "+ mBus + "  " + str(mminutes) + "m")
-lcd.set_cursor(0,1)
-lcd.message("Shady:  "+ sBus + "  " + str(sminutes) + "m")
+		# get current time 
+		cTime = datetime.now().time()
+
+		# convert times and calc # of minutes until arrival
+		mArrival = datetime.strptime(mArrival, "%Y%m%d %H:%M:%S")
+		sArrival = datetime.strptime(sArrival, "%Y%m%d %H:%M:%S")
+
+		sminutes = datetime.combine(date.today(), sArrival.time()) - datetime.combine(date.today(), cTime)
+		sminutes = math.floor(sminutes.total_seconds()/60)
+		sminutes = math.trunc(sminutes)
+
+		mminutes = datetime.combine(date.today(), mArrival.time()) - datetime.combine(date.today(), cTime)
+		mminutes = math.floor(mminutes.total_seconds()/60)
+		mminutes = math.trunc(mminutes)
+
+		#print data to lcd
+		lcd.set_cursor(0,0)
+		lcd.message("Murray: "+ mBus + "  " + str(mminutes) + "m")
+		lcd.set_cursor(0,1)
+		lcd.message("Shady:  "+ sBus + "  " + str(sminutes) + "m")
 
 
